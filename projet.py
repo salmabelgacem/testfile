c05f24d8e9c061d4643627c0140af0aba19e48d0
salma <salmabelgacem9@gmail.com>
2025-03-13 22:08:21 +0100

    la somme de fichier

--- projet.py
+++ projet.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 data=(123)
 if data :
 print ( "la somme ",sum(data))
@@ -6,3 +7,11 @@ print ("lle max"max(data))
 else:
 print ('dosier vide')
 
+=======
+
+def somme(T)
+S=0
+for t in T
+S+=t
+return S 
+>>>>>>> 13e46ff (la somme de fichier)
